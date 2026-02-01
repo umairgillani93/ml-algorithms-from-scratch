89778f80840ba4828db540f79c0e31f9f46ba641
umairgillani93 <umairgillani93@gmail.com>
2026-02-01 23:46:01 +0500

    matrix multiplication in C (professional way)

---
+++ matmul.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define RAND_FLOAT (float) rand() / (float) RAND_MAX;
+
+// Matrix structure
+struct Matrix {
+	int rows;
+	int cols;
+	int size;
+	float *data;
+};
+
+struct Matrix *matmul(struct Matrix *matrix1, struct Matrix *matrix2) {
+	// define matrix data
+	matrix1->rows = 3;
+	matrix1->cols = 1;
+	matrix1->size = matrix1->rows * matrix1->cols;
+	matrix1->data = malloc(matrix1->size * sizeof(float));
+
+	matrix2->rows = 1;
+	matrix2->cols = 4;
+	matrix2->size = matrix2->rows * matrix2->cols;
+	matrix2->data = malloc(matrix2->size * sizeof(float));
+
+	// assign values to the matrices
+	for (int i = 0; i < matrix1->rows; i++) {
+		for (int j = 0; j < matrix1->cols; j++) {
+			matrix1->data[i * matrix1->rows + j] = RAND_FLOAT;
+		}
+	}
+
+	for (int i = 0; i < matrix2->rows; i++) {
+		for (int j = 0; j < matrix2->cols; j++) {
+			matrix2->data[i * matrix2->rows + j] = RAND_FLOAT;
+		}
+	}
+
+	// Resultant Matrix
+	struct Matrix *result = malloc(sizeof(struct Matrix));
+	result -> rows = matrix1 -> rows;
+	result -> cols = matrix2 -> cols;
+	result -> size = matrix1 -> rows * matrix2 -> cols;
+	result -> data = malloc(result -> size * sizeof(float));
+
+	// Multiply the matrices
+	for (int i = 0; i < matrix1->rows; i++) {
+		for (int j = 0; j < matrix2->cols; j++) {
+			float sum = 0.0f;
+			for (int k = 0; k < matrix1->cols; k++) {
+				// Remember 'k' is shared dimention for multplication of matrix1 and matrix2
+				sum += matrix1->data[i * matrix1->rows + k] * matrix2->data[k * matrix2->cols + j];
+			}
+			result -> data[i * matrix1 -> rows + j] = (float) sum;
+		}
+	}
+	return result;
+}
+
+
+int main() {
+	// initilize structure pointer to the Matrix and allocate it's memory
+	struct Matrix *matrix1 = malloc(sizeof(struct Matrix));
+	struct Matrix *matrix2 = malloc(sizeof(struct Matrix));
+
+	struct Matrix *res = matmul(matrix1, matrix2);
+	for (int i = 0; i < res -> rows; i++) {
+		for (int j = 0; j < res -> cols; j++) {
+			printf("%f ",  res->data[i * res -> cols + j]);
+		}
+		printf("\n");
+	}
+	printf("All good\n");
+	return 0;
+}
+
