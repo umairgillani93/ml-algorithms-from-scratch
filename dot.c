e1b5481dbc4ca555c952881502969a4b06868e98
umairgillani93 <umairgillani93@gmail.com>
2026-02-03 16:17:29 +0500

    transpose of a Matrix

--- dot.c
+++ dot.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// Matrix structure
+struct Matrix {
+	int rows;
+	int cols;
+	int size;
+	int *data;
+};
+
 
 int dot(const int *a, int *b, const int size) {
 	int sum = 0;
@@ -8,13 +18,64 @@ int dot(const int *a, int *b, const int size) {
 	return sum;
 }
 
-int main() {
-	int A[] = {2,4,6,8};
-	int B[] = {1,3,7,9};
 
-	int res = dot(A, B, 4);
-	printf("%d\n", res);
-	return 0;
+struct Matrix *transpose(struct Matrix *matrix) {
+	struct Matrix *res = malloc(sizeof(struct Matrix));
+	res->rows = matrix->cols;
+	res->cols = matrix->rows;
+	res->size = res->rows * res->cols;
+	res->data = malloc(res->size * sizeof(int));
+	for (int i = 0; i < matrix->rows; i++) {
+		for (int j = 0; j < matrix->cols; j++) {
+			res->data[j * res->rows + i] = matrix->data[i * matrix->cols + j];
+		}
+	}
+	return res;
+}
+
+
+
+int main() {
+	struct Matrix *A = malloc(sizeof(struct Matrix));
+
+	A->rows = 3;
+	A->cols = 4;
+	A->size = A->rows  * A->cols;
+	A->data = malloc(A->size * sizeof(int));
+
+
+	// Initilize the matrices
+	for (int i = 0; i < A->rows; i++) {
+		for (int j = 0; j < A->cols; j++) {
+			A->data[i * A->cols + j] = (i + 1) * A->cols + j;
+		}
+	}
+
+	struct Matrix *final = transpose(A);
+
+	for (int i = 0; i < A->rows; i++) {
+		for (int j = 0; j < A->cols; j++) {
+			printf("%d ", A->data[i * A->cols + j]);
+		}
+		printf("\n");
+	}
+
+	printf("-------------------\n");
+
+	for (int i = 0; i < final->rows; i++) {
+		for (int j = 0; j < final->cols; j++) {
+			printf("%d ", final->data[i * final->cols + j]);
+		}
+		printf("\n");
+	}
+
+	//for (int i = 0; i < A->rows; i++) {
+	//	for (int j = 0; j < A->cols; j++) {
+	//		printf("%d ", A->data[i * A->cols + j]);
+	//	}
+	//	printf("\n");
+	//}
 	
+	return 0;
 }
 
