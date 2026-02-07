82d4ce934a2186399c4247f527fe7c8bd171f6e9
umairgillani93 <umairgillani93@gmail.com>
2026-02-08 01:16:44 +0500

    kadane's algorithm in C

--- dsa/main.c
+++ dsa/main.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
+#define MAX(a, b) ((a > b) ? (a) : (b))
+
 int main() {
 	int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
-
-	int curr_sum = arr[0];
+  // int curr_sum = arr[0];
+	
 	int best_sum = arr[0];
+	int overall_max= arr[0];
+
 	int SIZE = sizeof(arr) / sizeof(arr[0]);
 
 	for (int i = 1; i < SIZE; i++) {
-		curr_sum = arr[i];
-		if (curr_sum + best_sum > curr_sum) {
-			best_sum += curr_sum;
+		if (best_sum + arr[i] > arr[i]) {
+			best_sum += arr[i];	
 		}
 		else {
-			best_sum = curr_sum;
+			best_sum = arr[i];
 		}
-		printf("iter: %d,  curr_sum: %d, curr_sum + arr[i - 1]: %d, best_sum: %d\n", i, arr[i], arr[i] + arr[i -1], best_sum);
+		overall_max = MAX(overall_max, best_sum);
+		printf("iter: %d, arr[i]: %d, best_sum: %d\n", i, arr[i], overall_max);
 	}
+
+	printf("%d\n", overall_max);
 	return 0;
 }
