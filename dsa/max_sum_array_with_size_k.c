4075920b4e5fc53ef0d49351a790e0c1405252fd
umairgillani93 <umairgillani93@gmail.com>
2026-02-08 16:44:55 +0500

    maximum sum array size with K elements

--- dsa/max_sum_array_with_size_k.c
+++ dsa/max_sum_array_with_size_k.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
 int main() {
-	int arr[] = {1, 4, 2, 10, 23, 3, 1, 0, 20};
+	int arr[] = {0, 0, 0, 10, 23, -10, 1, 0, 0};
+	int SIZE = sizeof(arr) / sizeof(arr[0]);
+	
 	// maximum sum with 'k' elements
 	int k = 4;
 
 	int i = 0;
-	int j = 1;
-	int SIZE = 9;
-	int sum = 0;
-	int max_sum = 0;
+	int j = i + 1;
+	int best = arr[i];
+	int curr = arr[i];
 
-	while (i < j && j < SIZE) {
-		sum += (arr[i] + arr[j]);
-		j++;
-		if ((j - i) == k - 1) {
-				i++;
-				j = i + 1;	
+	while (j < SIZE) {
+		while (j - i < k - 1) {
+			curr += arr[j];
+			j++;
 		}
-		printf("iteration: %d, sum: %d, max_sum: %d, i: %d, j: %d\n", i, sum, max_sum, i, j);
-		max_sum = MAX(sum, max_sum);
+		best = MAX(curr, best);
+		i++;
+		curr = arr[i];
+		j = i + 1;
+		printf("i: %d, j: %d, curr: %d, best: %d\n", i, j, curr, best);
 	}
-	printf("%d\n", max_sum);
+	printf("%d\n", best);
 	return 0;
 }
