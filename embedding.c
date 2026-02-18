e28443bc81b4c37bf625354a682d08bda7fc96a0
umairgillani93 <umairgillani93@gmail.com>
2026-02-19 01:35:53 +0500

    Embedding model initialization

--- embedding.c
+++ embedding.c
@@ -5,16 +5,37 @@
 #define RAND_FLOAT  (float) rand() / (float) RAND_MAX
 
 typedef struct {
+	int rows;
+	int cols;
+	int size = rows * cols;
+	char dtype = 'int';
+	float *data;
+} Tensor;
+typedef struct {
 	int vocab_size;
 	int emb_dim;
 	float *weights;
 } Embedding;
 
-int main() {
-	srand(time(NULL));
+Tensor create_tensor(int rows, int cols) {
+	// TODO:
+}
+
+//Embedding *forward(Embedding *e, Tensor *tokens, Embedding *out) {
+//	int size = tokens->size;
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < e->emb_size; j++) {
+//			out[i * e->emb_dim + j] = e->weights[tokens[i] * e->emb_dim + j];
+//		}
+//	}
+//	return out;
+//}
+
+
+Embedding *create_embeddings(int vocab_size, int emb_dim) {
 	Embedding *emb = malloc(sizeof(Embedding));
-	emb->vocab_size = 3;
-	emb->emb_dim = 5;
+	emb->vocab_size = vocab_size;
+	emb->emb_dim = emb_dim;
 	emb->weights= malloc(emb->vocab_size * emb->emb_dim * sizeof(float));
 
 	for (int i = 0; i < emb->vocab_size; i++) {
@@ -23,12 +44,23 @@ int main() {
 		}
 	}
 
+	return emb;
+}
+
+
+int main() {
+	srand(time(NULL));
+	Embedding *emb = create_embeddings(3, 4);
+	Tensor *tokens = // TODO:
+	Embedding *res = forward(emb, tokens, out);
+	
 	for (int i = 0; i < emb->vocab_size; i++) {
 		for (int j = 0; j < emb->emb_dim; j++) {
 			printf("%f ", emb->weights[i * emb->emb_dim + j]);
 		}
 		printf("\n");
 	}
+
 	return 0;
 }
 
